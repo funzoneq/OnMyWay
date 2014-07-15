57f4c694827a7bdecac780a689a0c51023fabe2e
Arnoud Vermeer <a.vermeer@freshway.biz>
2014-07-15 13:40:00 +0200

    Basic intake script

---
+++ roles/base/files/intake.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'collins_auth'
+
+client = Collins::Authenticator.setup_client
+
+tag = `/usr/bin/ipmitool fru print | grep 'Asset Tag' | awk '{ print $5 }' | head -n1`
+lshw = File.read('/tmp/it-lshw-report.xml')
+lldp = File.read('/tmp/it-lldp-report.xml')
+
+# http://tumblr.github.io/collins/api.html#api-asset-get-section
+asset = client.get(tag, { :details => true })
+
+if not client.exists?(tag) or asset.get_attribute("reintake") == 'true'
+  # Create the asset in collins
+  # http://tumblr.github.io/collins/api.html#api-asset-create-section
+  asset = client.create!(tag, {:generate_ipmi => true, :type => "SERVER_NODE" })
+
+  # Post lshw and lldp information
+  # http://tumblr.github.io/collins/api.html#api-asset-update-section
+  client.set_multi_attribute!(tag, { "lldp" => lldp, "lshw" => lshw })
+end
+
+# Create a lock file so that we don't re-intake
+File.open("/var/db/intake.lock", 'w+') {|f| f.write("Remove me if you want to re-intake.") }
