ade62231d39ea3c8abd1dad57cee99c069255fe7
Arnoud Vermeer <a.vermeer@freshway.biz>
2014-11-26 17:40:24 +0100

    Succesful intake

--- roles/intake/files/intake.rb
+++ roles/intake/files/intake.rb
@@ -10,15 +10,19 @@ lldp = File.read('/tmp/it-lldp-report.xml')
 
 # http://tumblr.github.io/collins/api.html#api-asset-get-section
 asset = client.get(tag, { :details => true })
+newasset = false
 
-if not client.exists?(tag) or asset.get_attribute("reintake") == 'true'
+if not client.exists?(tag)
   # Create the asset in collins
   # http://tumblr.github.io/collins/api.html#api-asset-create-section
   asset = client.create!(tag, {:generate_ipmi => true, :type => "SERVER_NODE" })
+  newasset = true
+end
 
+if newasset or asset.get_attribute("reintake") == 'true'
   # Post lshw and lldp information
   # http://tumblr.github.io/collins/api.html#api-asset-update-section
-  client.set_multi_attribute!(tag, { "lldp" => lldp, "lshw" => lshw })
+  client.set_multi_attribute!(tag, { "lldp" => lldp, "lshw" => lshw, "CHASSIS_TAG" => tag })
 end
 
 # Create a lock file so that we don't re-intake
